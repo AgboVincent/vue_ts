5cbc26d36f19de46bae41f13facd6e70f07c4e4d
John Flash <Johngood4real@yahoo.co.in>
2021-12-16 16:33:11 +0100

    Translate  password reset components

--- src/pages/PasswordResetSuccessful.vue
+++ src/pages/PasswordResetSuccessful.vue
@@ -1,9 +1,9 @@
 <template>
   <img :src="'/images/password-reset-success.png'" alt="Password Reset Success Image" class="w-[96px] h-auto mx-auto"/>
-  <p class="text-lg font-bold text-text-dark text-center my-4">Password Created</p>
-  <p class="text-text-dark text-center">Your password has been successfully reset. </p>
+  <p class="text-lg font-bold text-text-dark text-center my-4">{{ $t("Password Created")}}</p>
+  <p class="text-text-dark text-center">{{ $t("Your password has been successfully reset")}}. </p>
 
-  <v-btn block to="/login" color="white" elevation="0" class="mt-10">Proceed to Login</v-btn>
+  <v-btn block to="/login" color="white" elevation="0" class="mt-10">{{ $t("Proceed to Login")}}</v-btn>
 </template>
 
 <script lang="ts">
